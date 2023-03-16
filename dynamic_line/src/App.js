99b7ce5efa16bbd38e7e54b296987eff3a4fe01e
Zapta <zapta@users.noreply.github.com>
2023-03-16 14:30:59 -0700

    Created a project for dynamic line.

--- dynamic_line/src/App.js
+++ dynamic_line/src/App.js
@@ -4,7 +4,7 @@ import ApexChart from './MyComponent';
 function App() {
   return (
     <div className="App">
-      <h1>Static Line</h1>
+      <h1>Dynamic Line</h1>
       <ApexChart />
     </div>
   );
